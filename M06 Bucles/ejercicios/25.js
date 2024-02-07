bd4292edbfea65df8d2d9d1fb72e508d9a536e85
MaximoStefanutti <stefanuttimaximo@gmail.com>
2024-02-07 02:19:03 -0300

    Modulo 6

--- M06 Bucles/ejercicios/25.js
+++ M06 Bucles/ejercicios/25.js
@@ -5,6 +5,16 @@ function esPalindromo(string) {
   // IMPORTANTE: Un palíndromo es una palabra o frase
   // que se lee igual hacia adelante que hacia atrás.
   // Tu código:
+
+  var palíndromo = string.replace(/ /g, "").toLowerCase();
+
+  var j = palíndromo.length - 1;
+
+  for(var i = 0; i < palíndromo.length; i++) {
+    if (palíndromo[i] !== palíndromo[j]) return false;
+    j--;
+  }
+  return true;
 }
 
 module.exports = esPalindromo;
