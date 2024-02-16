da8469ae9b98f4e72e0d6caaac13efdd9cae2bae
Maximo Stefanutti <stefanuttimaximo@gmail.com>
2024-02-15 22:04:38 -0300

    Modulo 7 terminado

--- M07 Arrays/ejercicios/30.js
+++ M07 Arrays/ejercicios/30.js
@@ -2,6 +2,14 @@ function encontrarElementoRepetido(numeros) {
   // La función recibe un argumento "numeros" que es un array de números.
   // Retorna el primer elemento repetido que se encuentre en el array.
   // Tu código:
+
+  numeros = numeros.sort();
+
+  for (let i = 0; i < numeros.length - 1; i++) {
+    if (numeros[i] === numeros[i + 1]) return numeros[i];
+  }
+
+  return undefined;
 }
 
 module.exports = encontrarElementoRepetido;
