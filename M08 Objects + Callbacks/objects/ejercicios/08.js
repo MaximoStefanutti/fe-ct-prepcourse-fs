63e84ab7b6b3d6bb127d5e3bcc819ba808216033
Maximo Stefanutti <stefanuttimaximo@gmail.com>
2024-02-21 01:57:37 -0300

    Modulo 8 Objects + Callback completo

--- M08 Objects + Callbacks/objects/ejercicios/08.js
+++ M08 Objects + Callbacks/objects/ejercicios/08.js
@@ -5,6 +5,15 @@ function crearGato(nombre, edad) {
   // La propiedad "meow" será una función que retorne el string: "Meow!".
   // Retornar el objeto.
   // Tu código:
+
+  const objeto = {
+    nombre: "Snowball",
+    edad: 1,
+    meow: function meow() {
+      return "Meow!";
+    },
+  };
+  return objeto;
 }
 
 module.exports = crearGato;
--- M08 Objects + Callbacks/objects/ejercicios/12.js
+++ M08 Objects + Callbacks/objects/ejercicios/12.js
@@ -2,6 +2,9 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
   // Debes multiplicar este número por 5 y retornar el resultado.
   // Tu código:
+  let respuesta = 5 * objetoMisterioso.numeroMisterioso;
+
+  return respuesta;
 }
 
 module.exports = multiplicarNumeroDesconocidoPorCinco;
